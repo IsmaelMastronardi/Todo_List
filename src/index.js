411f15b891f91ec993da2de14e243e972628faec
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-07-12 23:40:45 -0300

    add styles for text area

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import './style.css';
 import {
   tasks, todoList, displayList, addTask, inspectTask,
 } from './modules/addEditRemove.js';
+import arrow from './assets/images/backArrow.png';
 
 const addBtn = document.querySelector('#addBtn');
-const deleteBtn = document.querySelector('#clearBtn');
+
 addBtn.addEventListener('click', addTask);
 
 tasks.sort((a, b) => a.index - b.index);
 tasks.forEach((obj) => displayList(obj));
 
 todoList.addEventListener('click', inspectTask);
+
+ const addBtnImg = document.querySelector('#buttonImg');
+ addBtnImg.src = arrow;

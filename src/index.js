60f606b4383617678750a1e30ccbf8801accf548
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-07-12 23:41:30 -0300

    fix linters erros

--- src/index.js
+++ src/index.js
@@ -5,13 +5,12 @@ import {
 import arrow from './assets/images/backArrow.png';
 
 const addBtn = document.querySelector('#addBtn');
+const addBtnImg = document.querySelector('#buttonImg');
 
+addBtnImg.src = arrow;
 addBtn.addEventListener('click', addTask);
 
 tasks.sort((a, b) => a.index - b.index);
 tasks.forEach((obj) => displayList(obj));
 
 todoList.addEventListener('click', inspectTask);
-
- const addBtnImg = document.querySelector('#buttonImg');
- addBtnImg.src = arrow;

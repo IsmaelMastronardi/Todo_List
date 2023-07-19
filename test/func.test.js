1b678be3579d8725c88ab3f316b5aa4c723399ed
Juan Diego Guerra <jdxelric@gmail.com>
2023-07-19 15:55:40 -0500

    Implement Mock for Local Storage
    Co-Authored-By: IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>

--- test/func.test.js
+++ test/func.test.js
@@ -2,3 +2,23 @@ import {
   addTask,
   remove
 } from './func.test.js';
+
+const localStorageMock = (() => {
+  let store = {};
+
+  return {
+    store,
+    getItem: ((key) => store[key] || []),
+    setItem: ((key, value) => {
+      store[key] = JSON.stringify(value);
+    }),
+    removeItem: ((key) => {
+      delete store[key];
+    }),
+    clear: (() => {
+      store = {};
+    }),
+  };
+})();
+
+global.localStorage = localStorageMock;
